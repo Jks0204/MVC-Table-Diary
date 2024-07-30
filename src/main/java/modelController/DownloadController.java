55f132513301d1be6763ae361d9df8e3be2f80a6
Jks0204 <rhkstn5092@naver.com>
2024-07-31 00:07:53 +0900

    Add files via upload

---
+++ src/main/java/modelController/DownloadController.java
@@ -0,0 +1,46 @@
+package modelController;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fileupload.FileUtil;
+
+/**
+ * Servlet implementation class DownloadController
+ */
+@WebServlet("/download.do")
+public class DownloadController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public DownloadController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String ofile=request.getParameter("ofile");
+		String sfile=request.getParameter("sfile");
+
+		FileUtil.download(request, response, "/upload", sfile, ofile);
+		
+		String no=request.getParameter("no");
+		
+		DiaryDAO dao=new DiaryDAO();
+		dao.downCountPlus(no);
+		DiaryDTO dto=dao.selectview(no);
+		dao.close();
+		
+		request.setAttribute("dto", dto);
+		request.getRequestDispatcher("/View.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
